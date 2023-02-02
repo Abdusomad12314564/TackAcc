b05232e0f1bf1652567523ee335ec0d670b3eba9
AbduSomad <abdusomadkoshmamatov@gmail.com>
2023-02-02 18:00:54 +0600

    commit new

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
         System.out.println(acc.fullName+" Ваш баланс: "+Account.deposit(300)+" сом");
         System.out.println(acc.fullName+" Ваш баланс: "+Account.withdrawal(400)+" сом");
 
+
         Account acc2=new Account();
         acc.fullName="Чыңгыз Шаршекеев: ";
         System.out.println(acc.fullName+" Ваш баланс: "+Account.deposit(1000)+" сом");
@@ -15,7 +16,6 @@ public class Main {
         acc.fullName="Темирлан Жузукулов: ";
         System.out.println(acc.fullName+" Ваш баланс: "+Account.deposit(100)+" сом");
         System.out.println(acc.fullName+" Ваш баланс: "+Account.withdrawal(600)+" сом");
-
 
     }
 }
